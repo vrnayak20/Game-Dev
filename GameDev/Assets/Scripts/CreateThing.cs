d6ac1f3d6ae0461b124f3985059a7609781a34b1
Hrudith <69597791+HrudithL@users.noreply.github.com>
2023-03-06 09:38:02 -0600

    no

--- GameDev/Assets/Scripts/CreateThing.cs
+++ GameDev/Assets/Scripts/CreateThing.cs
@@ -9,7 +9,7 @@ public class CreateThing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("no");
     }
 
     // Update is called once per frame
