0d2b0a9a1898fbbe2ea723afce899a23fc010b75
Hrudith <69597791+HrudithL@users.noreply.github.com>
2023-03-06 20:06:59 -0600

    added banana script

---
+++ GameDev/Assets/Scripts/Banana.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banana : MonoBehaviour
+{
+    int i = 10;
+    float g = 3.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
